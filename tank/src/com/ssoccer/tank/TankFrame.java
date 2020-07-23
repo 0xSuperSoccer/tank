b2ce6ae096ffbbfea727e13a50172c039eaed52c
SuperSoccer <superjr.kel@163.com>
2020-07-23 20:20:16 +0800

    在tankFrame中加入了双缓冲，解决闪烁问题。

--- tank/src/com/ssoccer/tank/TankFrame.java
+++ tank/src/com/ssoccer/tank/TankFrame.java
@@ -1,7 +1,9 @@
 package com.ssoccer.tank;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
@@ -14,13 +16,13 @@ public class TankFrame extends Frame{
 	
 	Tank myTank = new Tank(200, 200, Dir.DOWN);
 	Bullet b = new Bullet(300, 300, Dir.DOWN);
-	
+	static final int GAME_WIDTH = 1600, GAME_HEIGHT = 1000;
 	
 	
 	
 	
 	public TankFrame() {
-		setSize(1600, 1000);
+		setSize(GAME_WIDTH, GAME_HEIGHT);
 		setResizable(false);
 		setTitle("Tank War");
 		setVisible(true);
@@ -36,6 +38,24 @@ public class TankFrame extends Frame{
 		});
 	}
 	
+	
+	
+	
+	
+	Image offScreenImage = null;
+	@Override
+	public void update(Graphics g) {
+		if(offScreenImage == null) {
+			offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+		}
+		Graphics gOffScreen = offScreenImage.getGraphics();
+		Color c = gOffScreen.getColor();
+		gOffScreen.setColor(Color.BLACK);
+		gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+		gOffScreen.setColor(c);
+		paint(gOffScreen);
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
 	
 	
 	
